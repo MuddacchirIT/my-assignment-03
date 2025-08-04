db32f18696dda19a0c00f86a2875bea6024bdeae
unknown <protexbeer41@gmail.com>
2025-08-04 13:40:31 +0600

    solved problem4

---
+++ problem4.js
@@ -0,0 +1,16 @@
+/** Problem 04 - (Delete / Store) */
+var fileName= "data.docx";
+//write your code here
+
+if(fileName === "data.docx"){
+    console.log("Store")
+}
+else if(fileName === "result.pdf"){
+    console.log("Store")
+}
+else if(fileName === "#exp.mp4"){
+    console.log("Store")
+}
+else{
+    console.log("Delete")
+}
